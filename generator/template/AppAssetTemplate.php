25247c99cdc587a9309300017b8f68dc75ce447d
bill-zhuang <zstu_bill@yahoo.com>
2015-03-05 17:50:27 +0800

    + finish app assset template

---
+++ generator/template/AppAssetTemplate.php
@@ -0,0 +1,38 @@
+<?php
+/* @var $module_name string module name */
+/* @var $controller_name string controller name */
+/* @var $css array css */
+/* @var $js array js */
+
+echo "<?php\n";
+?>
+
+namespace app\assets<?php echo ($module_name === '') ? '' : ('\\' . $module_name); ?>;
+
+use yii\web\AssetBundle;
+class AppAsset<?php echo $controller_name; ?> extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        <?php
+            foreach ($css as $value)
+            {
+                echo $value, ",\n";
+            }
+        ?>
+    ];
+    public $js = [
+        <?php
+            foreach ($js as $value)
+            {
+                echo $value, ",\n";
+            }
+        ?>
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset', //bootstrap css
+        'yii\bootstrap\BootstrapPluginAsset', //bootstrap js
+    ];
+}
