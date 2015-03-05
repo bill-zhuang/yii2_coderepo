e1fd3d20c37dd9ea9863c41e1c99d3995b40966d
bill-zhuang <zstu_bill@yahoo.com>
2015-03-05 17:50:50 +0800

    + finish module template

---
+++ generator/template/ModuleTemplate.php
@@ -0,0 +1,15 @@
+<?php
+/* @var $module_name string module name */
+
+echo "<?php\n";
+?>
+
+namespace app\modules\<?php echo $module_name; ?>;
+
+class <?php echo $module_name; ?>Module extends \yii\base\Module
+{
+    public function init()
+    {
+        parent::init();
+    }
+}
