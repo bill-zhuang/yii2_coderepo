6595961f3622597d8b99149b6753857d9a86e63e
bill-zhuang <zstu_bill@yahoo.com>
2015-03-07 18:05:27 +0800

    + update controller template

--- generator/template/ControllerTemplate.php
+++ generator/template/ControllerTemplate.php
@@ -10,12 +10,12 @@
 echo "<?php\n";
 ?>
 
-namespace app\<?php echo ($module_name === '') ? '' : ('modules\\' . $module_name); ?>\models\controllers;
+namespace app\<?php echo ($module_name === '') ? '' : ('modules\\' . strtolower($module_name)); ?>\controllers;
 
 use yii;
 use yii\web\Controller;
 <?php foreach($model_names as $model_name){ ?>
-use app\<?php echo ($module_name === '') ? '' : ('modules\\' . $module_name); ?>\models\<?php echo $model_name; ?>;<?php echo PHP_EOL; ?>
+use app\<?php echo ($module_name === '') ? '' : ('modules\\' . strtolower($module_name)); ?>\models\<?php echo $model_name; ?>;<?php echo PHP_EOL; ?>
 <?php } ?>
 
 class <?php echo $controller_name; ?>Controller extends Controller
