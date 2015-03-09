619cdb415080960c5e557f3faedfc97b54e0c6be
bill-zhuang <zstu_bill@yahoo.com>
2015-03-09 19:25:21 +0800

    + yii2 config files

---
+++ config/params.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'adminEmail' => 'admin@example.com',
+    //controller global variable
+    'init_start_page' => 1,
+    'init_page_length' => 25,
+    'init_total_page' => 1,
+    'valid_status' => 1,
+    'invalid_status' => 0,
+    'init_affected_rows' => 0,
+    'invalid_primary_id' => 0,
+];
