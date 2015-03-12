9c13d81b6296b1f4c0d66672c63b548a35ee9300
bill-zhuang <zstu_bill@yahoo.com>
2015-03-12 15:50:18 +0800

    + finish bad history chart function(chart js need to added)

---
+++ assets/Person/AppAssetBadHistoryChart.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\assets\person;
+
+use yii\web\AssetBundle;
+class AppAssetBadHistoryChart extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+
+    ];
+    public $js = [
+        'js/person/bad-history-chart/bad-history-chart.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset', //bootstrap css
+        'yii\bootstrap\BootstrapPluginAsset', //bootstrap js
+    ];
+}
