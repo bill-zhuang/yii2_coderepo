af821b5fa0933108ad694c93123797ce2bf8449e
bill-zhuang <zstu_bill@yahoo.com>
2015-03-26 18:18:57 +0800

    + login layout & assets

---
+++ views/layouts/layout-login.php
@@ -0,0 +1,25 @@
+<?php
+/* @var $content string */
+use yii\helpers\Html;
+use app\assets\AppAssetLayoutLogin;
+AppAssetLayoutLogin::register($this);
+?>
+
+<?php $this->beginPage() ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="zh-cn">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <?php Html::csrfMetaTags(); ?>
+        <title>后台</title>
+        <?php $this->head() ?>
+    </head>
+    <body>
+    <?php $this->beginBody() ?>
+        <div class="login">
+            <?php echo $content; ?>
+        </div>
+    <?php $this->endBody() ?>
+    </body>
+</html>
+<?php $this->endPage() ?>
