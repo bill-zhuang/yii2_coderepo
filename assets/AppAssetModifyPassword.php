973b2bf2233456247898754a9c0df5ddddfe2322
bill-zhuang <zstu_bill@yahoo.com>
2016-06-15 14:07:21 +0800

    + update asset of modify password

--- assets/AppAssetModifyPassword.php
+++ assets/AppAssetModifyPassword.php
@@ -11,10 +11,12 @@ class AppAssetModifyPassword extends AssetBundle
 
     ];
     public $js = [
-        'js/main/modify-password.js',
+        'js/default/modify-password.js',
+        'js/common/jAjaxWidget.js',
     ];
     public $depends = [
         'yii\bootstrap\BootstrapAsset', //bootstrap css
         'yii\bootstrap\BootstrapPluginAsset', //bootstrap js
+        'app\assets\plugins\jQuerySerializeObjectAsset',
     ];
 }
