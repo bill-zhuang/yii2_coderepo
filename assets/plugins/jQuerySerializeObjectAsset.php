bbe31c4300b3cbb0c88e34274781d294ee0f247d
bill-zhuang <zstu_bill@yahoo.com>
2016-06-21 15:32:59 +0800

    + add jquery serialize object lib

--- assets/plugins/jQuerySerializeObjectAsset.php
+++ assets/plugins/jQuerySerializeObjectAsset.php
@@ -5,7 +5,7 @@ namespace app\assets\plugins;
 use yii\web\AssetBundle;
 class jQuerySerializeObjectAsset extends AssetBundle
 {
-    public $sourcePath = 'assets/jquery-serialize-object/js';
+    public $sourcePath = 'js/jquery-serialize-object/js';
     public $baseUrl = '@web';
     public $css = [
 
