1d214908d397e2cc1110ab72e6b5b0c41ea10724
bill-zhuang <zstu_bill@yahoo.com>
2016-06-21 16:39:19 +0800

    + finish send mail feature

--- library/bill/Mail.php
+++ library/bill/Mail.php
@@ -5,13 +5,10 @@ namespace app\library\bill;
 class Mail
 {
     private $_username = 'your@mail.com';
-    private $_password = 'your_password';
-    private $_host = 'smtp.mail.com';
-    private $_port = 25;
     private $_receiver = 'receive@mail.com';
     private $_bill_server_host = 'production.host';
 
-    public function send($title, $body, $receiver = null, $attachment = null, $charset = 'utf-8')
+    public function send($title, $body, $receiver = null, $attachment = null)
     {
         $server_host = $_SERVER['HTTP_HOST'];
         if ($server_host != $this->_bill_server_host)
@@ -19,15 +16,31 @@ class Mail
             return true;
         }
 
-        $config = [
-            'auth'=>'login',
-            'username'=> $this->_username,
-            'password'=> $this->_password,
-            //'ssl' => 'ssl',
-            'port' => $this->_port
-        ];
-        //TODO
+        $env = Util::isProductionEnv() ? 'product' : 'alpha';
+        $title = '(' . \Yii::$app->user->identity->name . '-' . $env . ')' . $title;
+        $title = '=?UTF-8?B?' . base64_encode($title) . '?=';
+        $receivers = $this->_initReceivers($receiver);
+        \Yii::$app->mailer->compose()
+            ->setFrom($this->_username)
+            ->setTo($receivers)
+            ->setSubject($title)
+            ->setTextBody($body)
+            ->attach($attachment)
+            ->send();
 
         return true;
+    }
+
+    private function _initReceivers($receiver)
+    {
+        if (is_array($receiver) && count($receiver) == 0) {
+            return [$this->_receiver];
+        }
+
+        if ($receiver == null) {
+            return [$this->_receiver];
+        }
+
+        return $receiver;
     }
 } 
