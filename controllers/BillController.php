3cc982dc3354a2d93423f9e29f1b9b03f9d360e8
bill-zhuang <zstu_bill@yahoo.com>
2016-06-30 11:10:03 +0800

    + add sql query trigger after action

--- controllers/BillController.php
+++ controllers/BillController.php
@@ -6,6 +6,8 @@ use yii;
 use yii\web\Controller;
 use app\models\Acl;
 use yii\web\Response;
+use app\library\bill\Constant;
+use app\library\bill\Util;
 
 class BillController extends Controller
 {
@@ -32,6 +34,38 @@ class BillController extends Controller
 
     public function afterAction($action, $result)
     {
-        return parent::afterAction($action, $result);
+        $result = parent::afterAction($action, $result);
+        //trigger sql query info
+        $sqlInfo = $this->_dbProfiling();
+        if (!Util::isProductionEnv() && yii::$app->request->get('sql', 0) == 1) {
+            echo '<pre>';
+            print_r($sqlInfo);
+            exit;
+        }
+        if (isset($sqlInfo['queryCost']) && $sqlInfo['queryCost'] > Constant::SQL_QUERY_COST_TRIGGER) {
+            //TODO slow query trigger
+        }
+        return $result;
+    }
+
+    //update from Yii::getLogger()->getDbProfiling()
+    private function _dbProfiling()
+    {
+        $sqlInfo = [
+            'queryCount' => 0,
+            'queryCost' => 0,
+            'queries' => [],
+        ];
+        $timings = Yii::getLogger()->getProfiling(['yii\db\Command::query', 'yii\db\Command::execute']);
+        $sqlInfo['queryCount'] = count($timings);
+        foreach ($timings as $timing) {
+            $sqlInfo['queryCost'] += $timing['duration'];
+            $sqlInfo['queries'][] = [
+                'sql' => $timing['info'],
+                'queryCost' => $timing['duration'],
+            ];
+        }
+
+        return $sqlInfo;
     }
 }
